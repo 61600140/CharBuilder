08b031968e2811bca20470c25f1628f0a95149ce
Jack Boes <jaybominecraft@gmail.com>
2023-03-14 08:49:51 -0400

    no

--- CharBuilder/MainWindow.xaml.cs
+++ CharBuilder/MainWindow.xaml.cs
@@ -20,9 +20,57 @@ namespace CharBuilder
     /// </summary>
     public partial class MainWindow : Window
     {
+        private RPGChar _character = new RPGChar();
+        private Random _rng = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+            updateStats();
+        }
+
+        private void buttonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            //update name
+            _character.Name = textBoxName.Text;
+        }
+
+        private void buttonRoll_Click(object sender, RoutedEventArgs e)
+        {
+            _character.Roll();
+            updateStats();
+            double odds = .5;
+
+            _character.PartyMembers.Clear();
+            foreach(ListBoxItem i in listPotentialMembers.Items)
+            {
+                if (_rng.NextDouble() > odds)
+                {
+                    RPGChar r = new RPGChar()
+                    {
+                        Name = i.Content.ToString(),
+                    };
+
+
+                    _character.PartyMembers.Add(r);
+                }
+            }
+            listMembers.Items.Clear();
+            foreach (RPGChar r in _character.PartyMembers)
+            {
+                ListBoxItem i = new ListBoxItem();
+                i.Content = $"{r.Name} \n STR: {r.Strength} \n INT: {r.Intelligence}";
+                listMembers.Items.Add(i);
+            }
+        }
+        private void updateStats()
+        {
+            textBoxStr.Text = _character.Strength.ToString();
+            textBoxDex.Text = _character.Dexterity.ToString();
+            textBoxInt.Text = _character.Intelligence.ToString();
+            textBoxChr.Text = _character.Charisma.ToString();
+            textBoxWis.Text = _character.Wisdom.ToString();
+            textBoxLuk.Text = _character.Luck.ToString();
         }
     }
 }
--- CharBuilder/RPGChar.cs
+++ CharBuilder/RPGChar.cs
@@ -9,18 +9,38 @@ namespace CharBuilder
 {
     internal class RPGChar
     {
-        // Fields
+        #region Fields
+        private CharacterClasses _characterClass;
         private int _charisma;
+        private int _maxCharisma = 20;
         private int _dexterity;
+        private int _maxDexterity = 20;
         private int _intelligence;
+        private int _maxIntelligence = 20;
         private int _luck;
+        private int _maxLuck = 20;
         private int _strength;
+        private int _maxStrength = 20;
         private int _wisdom;
+        private int _maxWisdom = 20;
+        private List<RPGChar> _partyMembers = new List<RPGChar>();
 
-        // Properties
-        //name & level
+        private Random _rng = new Random();
+        #endregion
+        public RPGChar()
+        {
+            Roll();
+        }
+        #region Properties
+        //tl vars
         public string Name { get; set; }
         public int Level { get; set; }
+        public CharacterClasses CharacterClasses { get { return _characterClass; } set { _characterClass = value; } }
+        public List<RPGChar> PartyMembers
+        {
+            get { return _partyMembers; }
+            set { _partyMembers = value; }
+        }
         //stats
         public int Charisma { get { return _charisma; } }
         public int Dexterity { get { return _dexterity; } }
@@ -28,8 +48,40 @@ namespace CharBuilder
         public int Luck { get { return _luck; } }
         public int Strength { get { return _strength; } }
         public int Wisdom { get { return _wisdom; } }
+        #endregion
 
+        public void Roll()
+        {
+            _charisma = _rng.Next(1, _maxCharisma+1);
+            _dexterity = _rng.Next(1, _maxDexterity + 1);
+            _intelligence = _rng.Next(1, _maxIntelligence + 1);
+            _luck = _rng.Next(1, _maxLuck + 1);
+            _strength = _rng.Next(1, _maxStrength + 1);
+            _wisdom = _rng.Next(1, _maxWisdom + 1);
 
+        }
 
+        public static int RollDice(int numberOfDice, int numberOfSides)
+        {
+            Random r = new Random();
+            int total = 0;
+
+            for(int i=0; i < numberOfDice; i++)
+            {
+                total += r.Next(1, numberOfSides+1);
+            }
+
+            return total;
+        }
+
+    }
+
+    public enum CharacterClasses
+    {
+        None = 0,
+        Cleric,
+        Rogue,
+        Barbarian,
+        Wizard
     }
 }
